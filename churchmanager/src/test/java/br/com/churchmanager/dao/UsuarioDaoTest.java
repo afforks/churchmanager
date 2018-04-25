4b0caa895a5d69e633895886cf356080a8c0b089
junior <junior.getsoftware@gmail.com>
2018-04-25 11:26:09 -0300

    Refatoração de testes (UsuarioDaoTest)

--- churchmanager/src/test/java/br/com/churchmanager/dao/UsuarioDaoTest.java
+++ churchmanager/src/test/java/br/com/churchmanager/dao/UsuarioDaoTest.java
@@ -1,11 +1,12 @@
 package br.com.churchmanager.dao;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -21,12 +22,28 @@ import br.com.churchmanager.util.JPAUtil;
 public class UsuarioDaoTest {
 
 	private static final boolean DISTINCT_TRUE = true;
+	private static final boolean ASC = true;
+
 	private EntityManager entityManager;
 	private UsuarioDAO usuarioDAO;
 	private PerfilDAO perfilDAO;
-	
+	private Perfil perfil;
+	private Usuario usuario;
+
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
+
+	private Perfil novoPerfil() {
+		Perfil perfil = new PerfilBuilder().comNome("Admin").comDescricao("Perfil de administrador").ativo().builder();
+		perfilDAO.salvar(perfil);
+		return perfil;
+	}
+
+	private Usuario novoUsuario() {
+		Usuario usuario = new UsuarioBuilder().comNome("Usuário de Teste").comEmail("usuario@teste.com.br")
+				.comPerfil(perfil).comSenha("123").build();
+		return usuario;
+	}
 
 	@Before
 	public void setUp() {
@@ -34,6 +51,8 @@ public class UsuarioDaoTest {
 		entityManager.getTransaction().begin();
 		usuarioDAO = new UsuarioDAO(entityManager);
 		perfilDAO = new PerfilDAO(entityManager);
+		perfil = novoPerfil();
+		usuario = novoUsuario();
 	}
 
 	@After
@@ -41,124 +60,128 @@ public class UsuarioDaoTest {
 		entityManager.getTransaction().rollback();
 		entityManager.close();
 		usuarioDAO = null;
+		perfilDAO = null;
+		perfil = null;
+		usuario = null;
 	}
 
 	@Test
 	public void deveCadastrarUsuario() {
-		Perfil perfil = new PerfilBuilder().comNome("Admin").builder();
-		perfilDAO.salvar(perfil);
+
 		long qtdRegistros = usuarioDAO.contagem("id", null, null, DISTINCT_TRUE);
-		Usuario usuario = new UsuarioBuilder().comNome("Usuário de Teste").comEmail("usuario@teste.com.br")
-				.comPerfil(perfil).comSenha("123").build();
 		usuarioDAO.salvar(usuario);
 		long totalAtualRegistros = usuarioDAO.contagem("id", null, null, DISTINCT_TRUE);
-		
+
 		Usuario usuarioDoBanco = usuarioDAO.buscarPorId(usuario.getId());
-		
-		Assert.assertEquals(qtdRegistros + 1, totalAtualRegistros);
+
+		assertEquals(qtdRegistros + 1, totalAtualRegistros);
 		assertEquals("id", usuario.getId(), usuarioDoBanco.getId());
 		assertEquals("nome", usuario.getNomeCompleto(), usuarioDoBanco.getNomeCompleto());
 		assertEquals("email", usuario.getEmail(), usuarioDoBanco.getEmail());
-		assertEquals("senha", usuario.getEmail(), usuarioDoBanco.getEmail());
+		assertEquals("senha", usuario.getSenha(), usuarioDoBanco.getSenha());
 		assertEquals("perfil", usuario.getPerfil(), usuarioDoBanco.getPerfil());
 
 	}
 
 	@Test
 	public void deveEditarUsuario() {
-		Perfil perfil = new PerfilBuilder().comNome("Admin").builder();
-		perfilDAO.salvar(perfil);
-		Usuario usuario = new UsuarioBuilder().comNome("Usuário de Teste").comEmail("usuario@teste.com.br")
-				.comPerfil(perfil).comSenha("123").build();
 		usuarioDAO.salvar(usuario);
+
 		usuario.setNomeCompleto("Nome editado");
 		usuario.setEmail("email@editado.com");
+
 		usuarioDAO.atualizar(usuario);
+
 		Usuario usuarioDoBanco = usuarioDAO.porEmail("email@editado.com");
-		
+
 		assertEquals("id", usuario.getId(), usuarioDoBanco.getId());
 		assertEquals("nome", usuario.getNomeCompleto(), usuarioDoBanco.getNomeCompleto());
 		assertEquals("email", usuario.getEmail(), usuarioDoBanco.getEmail());
-		assertEquals("senha", usuario.getEmail(), usuarioDoBanco.getEmail());
+		assertEquals("senha", usuario.getSenha(), usuarioDoBanco.getSenha());
 		assertEquals("perfil", usuario.getPerfil(), usuarioDoBanco.getPerfil());
 
 	}
 
 	@Test
 	public void deveRemoverUsuario() {
-		Perfil perfil = new PerfilBuilder().comNome("Admin").builder();
-		perfilDAO.salvar(perfil);
-		Usuario usuario = new UsuarioBuilder().comNome("Usuário de Teste").comEmail("usuario@teste.com.br")
-				.comPerfil(perfil).comSenha("123").build();
 		usuarioDAO.salvar(usuario);
 		long qtdRegistros = usuarioDAO.contagem("id", null, null, DISTINCT_TRUE);
 		usuarioDAO.excluir(usuario);
 		long totalAtualRegistros = usuarioDAO.contagem("id", null, null, DISTINCT_TRUE);
 		usuario = usuarioDAO.buscarPorId(usuario.getId());
-		Assert.assertNull(usuario);
-		Assert.assertEquals(qtdRegistros - 1, totalAtualRegistros);
+		assertNull(usuario);
+		assertEquals(qtdRegistros - 1, totalAtualRegistros);
 	}
 
 	@Test
-	public void deveListarUsuarios() {
+	public void deveListarUsuariosOrdenandoPorNome() {
+		Usuario usuario1 = new UsuarioBuilder().comNome("Usuário Um").comEmail("usuario@um.com.br").comPerfil(perfil)
+				.comSenha("111").build();
+		Usuario usuario2 = new UsuarioBuilder().comNome("Usuário Dois").comEmail("usuario@dois.com.br")
+				.comPerfil(perfil).comSenha("222").build();
+		Usuario usuario3 = new UsuarioBuilder().comNome("Usuário Três").comEmail("usuario@tres.com.br")
+				.comPerfil(perfil).comSenha("333").build();
+
+		usuarioDAO.salvar(usuario1);
+		usuarioDAO.salvar(usuario2);
+		usuarioDAO.salvar(usuario3);
+
+		List<Usuario> usuarios = usuarioDAO.listar(ASC, "nomeCompleto");
+		int tamanhoDaLista = usuarios.size();
+
+		assertEquals(3, tamanhoDaLista);
+		assertEquals("Usuário Dois", usuarios.get(0).getNomeCompleto());
+		assertEquals("Usuário Três", usuarios.get(1).getNomeCompleto());
+		assertEquals("Usuário Um", usuarios.get(2).getNomeCompleto());
 
 	}
 
 	@Test
 	public void deveBuscarUsuarioPorIdentificador() {
-		Perfil perfil = new PerfilBuilder().comNome("Admin").builder();
-		perfilDAO.salvar(perfil);
-		Usuario usuario = new UsuarioBuilder().comNome("Usuário de Teste").comEmail("usuario@teste.com.br")
-				.comPerfil(perfil).comSenha("123").build();
+
 		usuarioDAO.salvar(usuario);
 		Usuario usuarioDoBanco = usuarioDAO.buscarPorId(usuario.getId());
-		
+
 		assertEquals("id", usuario.getId(), usuarioDoBanco.getId());
 		assertEquals("nome", usuario.getNomeCompleto(), usuarioDoBanco.getNomeCompleto());
 		assertEquals("email", usuario.getEmail(), usuarioDoBanco.getEmail());
-		assertEquals("senha", usuario.getEmail(), usuarioDoBanco.getEmail());
+		assertEquals("senha", usuario.getSenha(), usuarioDoBanco.getSenha());
 		assertEquals("perfil", usuario.getPerfil(), usuarioDoBanco.getPerfil());
 
 	}
 
 	@Test
 	public void deveEncontrarUsuarioPorEmail() {
-		Perfil perfil = new PerfilBuilder().comNome("Admin").builder();
-		perfilDAO.salvar(perfil);
-		Usuario usuario = new UsuarioBuilder().comNome("Usuário de Teste").comEmail("usuario@teste.com.br")
-				.comPerfil(perfil).comSenha("123").build();
+
 		usuarioDAO.salvar(usuario);
 		Usuario usuarioDoBanco = usuarioDAO.porEmail("usuario@teste.com.br");
-		
+
 		assertEquals("id", usuario.getId(), usuarioDoBanco.getId());
 		assertEquals("nome", usuario.getNomeCompleto(), usuarioDoBanco.getNomeCompleto());
 		assertEquals("email", usuario.getEmail(), usuarioDoBanco.getEmail());
-		assertEquals("senha", usuario.getEmail(), usuarioDoBanco.getEmail());
+		assertEquals("senha", usuario.getSenha(), usuarioDoBanco.getSenha());
 		assertEquals("perfil", usuario.getPerfil(), usuarioDoBanco.getPerfil());
 	}
 
 	@Test
 	public void deveRetornarNuloSeNaoEncontrarUsuarioPorEmail() {
 		Usuario usuarioDoBanco = usuarioDAO.porEmail("usuario@teste.com.br");
-		Assert.assertNull(usuarioDoBanco);
+		assertNull(usuarioDoBanco);
 	}
-	
+
 	@Test
 	public void naoDeveTerEmailDuplicado() {
 		expectedException.expect(ViolacaoDeRestricaoException.class);
 
-		Perfil perfil = new PerfilBuilder().comNome("Admin").builder();
-		perfilDAO.salvar(perfil);
-		
 		Usuario usuario1 = new UsuarioBuilder().comNome("Usuário de Teste 1").comEmail("usuario@teste.com.br")
 				.comPerfil(perfil).comSenha("123").build();
 
 		Usuario usuario2 = new UsuarioBuilder().comNome("Usuário de Teste 2").comEmail("usuario@teste.com.br")
 				.comPerfil(perfil).comSenha("456").build();
-		
+
 		usuarioDAO.salvar(usuario1);
 		usuarioDAO.salvar(usuario2);
-		
+
 	}
 
 }
